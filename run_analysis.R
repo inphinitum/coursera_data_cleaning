7d937bd389c26d66dfab0504b5412991866a7aa3
Javier Martinez <javi@flamingalah.net>
2020-05-17 15:59:03 +0200

    Extract only mean and standard deviation variables for each measurement

--- run_analysis.R
+++ run_analysis.R
@@ -4,10 +4,20 @@ FUCI_HAR_Dataset_Filename <- "data/FUCI_HAR.zip"
 download.file(FUCI_HAR_Dataset_URL, FUCI_HAR_Dataset_Filename)
 unzip(FUCI_HAR_Dataset_Filename, exdir = "data")
 
-# Merge training and test sets
+### STEP 1: Merge training and test sets
 # Data is stored in test/X_test.txt and train/X_train.txt
 testDataFilename <- "data/UCI HAR Dataset/test/X_test.txt"
 trainDataFilename <- "data/UCI HAR Dataset/train/X_train.txt"
 testData <- read.table(testDataFilename)
 trainData <- read.table(trainDataFilename)
 fullData <- rbind(testData, trainData)
+
+### STEP 2: Extract only mean and standard deviation for each measurement
+#
+# We're going to read the features and filter by mean() and std(), so we get the variable indexes we want
+featuresFilename <- "data/UCI HAR Dataset/features.txt"
+features <- read.table(featuresFilename)
+indexesMeanAndStdVariables <- grep("std()|mean()", features$V2)
+
+# Filter full dataset to only contain the variables that contain mean and standard deviation measurements
+fullDataMeanStd <- fullData[, indexesMeanAndStdVariables]
