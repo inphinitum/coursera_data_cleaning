a6a3c78f86ebb08e29c7e00f6edb13bfef045ffd
Javier Martinez <javi@flamingalah.net>
2020-05-17 15:48:06 +0200

    Merge train and test datasets into one

--- run_analysis.R
+++ run_analysis.R
@@ -3,3 +3,11 @@ FUCI_HAR_Dataset_URL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fproject
 FUCI_HAR_Dataset_Filename <- "data/FUCI_HAR.zip"
 download.file(FUCI_HAR_Dataset_URL, FUCI_HAR_Dataset_Filename)
 unzip(FUCI_HAR_Dataset_Filename, exdir = "data")
+
+# Merge training and test sets
+# Data is stored in test/X_test.txt and train/X_train.txt
+testDataFilename <- "data/UCI HAR Dataset/test/X_test.txt"
+trainDataFilename <- "data/UCI HAR Dataset/train/X_train.txt"
+testData <- read.table(testDataFilename)
+trainData <- read.table(trainDataFilename)
+fullData <- rbind(testData, trainData)
