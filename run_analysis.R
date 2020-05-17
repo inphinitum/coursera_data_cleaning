7d185acccf932f5b0c9f6967c4d547aa51ef66dd
Javier Martinez <javi@flamingalah.net>
2020-05-17 18:11:18 +0200

    Moved step 2 and 4 to earlier in the code, implemented step 5: summary per user and activity

--- run_analysis.R
+++ run_analysis.R
@@ -11,11 +11,27 @@ unzip(FUCI_HAR_Dataset_Filename, exdir = "data")
 # Labels are stored in test/y_test.txt and train/y_train.txt
 # Subjects are stored in test/subject_test.txt and train/subject_train.txt
 
+# NOTE: We're doing step 4 from the beginning since it simplifies things later on.
+# Read variable names here, so we can assign them as soon as we've read the test and train data sets.
+featuresFilename <- "data/UCI HAR Dataset/features.txt"
+features <- read.table(featuresFilename)
+
+# Convert factor to strings, so we can assign them later to variable names
+features <- sapply(features$V2, as.character)
+
 # Load measurement, activity and subject data for test data
 testDataFilename <- "data/UCI HAR Dataset/test/X_test.txt"
 testDataLabelsFilename <- "data/UCI HAR Dataset/test/y_test.txt"
 testDataSubjectsFilename <- "data/UCI HAR Dataset/test/subject_test.txt"
 testData <- read.table(testDataFilename)
+
+# Assign descriptive variable names as per step 4 in the instructions
+names(testData) <- features
+
+# Offset the data as per step 2 in the instructions
+indexesMeanAndStdVariables <- grep("std()|mean()", features)
+testData <- testData[, indexesMeanAndStdVariables]
+
 testDataLabels <- read.table(testDataLabelsFilename)
 names(testDataLabels) <- "Activity"
 testDataSubjects <- read.table(testDataSubjectsFilename)
@@ -27,6 +43,13 @@ trainDataFilename <- "data/UCI HAR Dataset/train/X_train.txt"
 trainDataLabelsFilename <- "data/UCI HAR Dataset/train/y_train.txt"
 trainDataSubjectsFilename <- "data/UCI HAR Dataset/train/subject_train.txt"
 trainData <- read.table(trainDataFilename)
+
+# Assign descriptive variable names as per step 4 in the instructions
+names(trainData) <- features
+
+# Offset the data as per step 2 in the instructions
+trainData <- trainData[, indexesMeanAndStdVariables]
+
 trainDataLabels <- read.table(trainDataLabelsFilename)
 names(trainDataLabels) <- "Activity"
 trainDataSubjects <- read.table(trainDataSubjectsFilename)
@@ -39,13 +62,7 @@ fullData <- rbind(testData, trainData)
 ###########
 ### STEP 2: Extract only mean and standard deviation for each measurement
 ###########
-# We're going to read the features and filter by mean() and std(), so we get the variable indexes we want
-featuresFilename <- "data/UCI HAR Dataset/features.txt"
-features <- read.table(featuresFilename)
-indexesMeanAndStdVariables <- grep("std()|mean()", features$V2)
-
-# Filter full dataset to only contain the variables that contain mean and standard deviation measurements
-fullDataMeanStd <- fullData[, indexesMeanAndStdVariables]
+# Already done earlier in step 1
 
 ###########
 ### STEP 3: Use descriptive activity names to name the activities in the dataset
@@ -56,3 +73,18 @@ activityLabels <- read.table(activityLabelsFilename)
 
 #  Convert fullData's Activity variable into a categorical one and assign labels according to read data
 fullData$Activity <- factor(fullData$Activity, labels=activityLabels$V2)
+
+#########
+# STEP 4: Label data set with descriptive variable names
+#########
+# Already done earlier in step 1.
+
+###########
+### Step 5: Create a second, independent tidy dataset with the average of each variable for each activity
+#           and each subject
+###########
+library(dplyr)
+meansPerActivityAndUser <- fullData %>%
+  group_by(Subject, Activity) %>%
+  summarize_all(mean)
+          
